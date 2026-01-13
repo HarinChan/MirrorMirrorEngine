5f17a2c05bdc35a6162429919d28dbb52ce618c3
HarinChan <harin.c325@gmail.com>
2026-01-13 12:50:56 +0000

    Copied Chroma service

--- src/app/router/profile.py
+++ src/app/router/profile.py
@@ -9,6 +9,7 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from src.app.main import db
 from src.app.model.account import Account
 from src.app.helper import PenpalsHelper
+from src.app.chromadb.chromadb_service import ChromaDBService
 
 
 profile_bp = Blueprint('profile', __name__)
