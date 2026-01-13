8592646ecc41c717454b40088124439445f7fe6d
HarinChan <harin.c325@gmail.com>
2026-01-13 12:44:36 +0000

    Restored main

--- src/app/main.py
+++ src/app/main.py
@@ -1,1 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+
+# app is for /router
+# db is for /model
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+app.secret_key = 'your_secret_key'  # Change this to something secure!
+
+db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()  # Create database tables
+
+app.run(debug=True)
